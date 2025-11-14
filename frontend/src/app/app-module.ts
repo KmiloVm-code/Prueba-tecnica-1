bbb818bcdbfe0de3626f2d7a448925cb6e08766f
KmiloVm <kmilo7v@gmail.com>
2025-11-14 16:23:57 -0500

    feat(app): add dashboard component and configure routing with guards

--- frontend/src/app/app-module.ts
+++ frontend/src/app/app-module.ts
@@ -1,21 +1,28 @@
-import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { LoginModule } from './features/login/login-module';
+import { RegisterModule } from './features/register/register-module';
+import { Dashboard } from './features/dashboard/dashboard';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Dashboard
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LoginModule,
+    RegisterModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-    provideClientHydration(withEventReplay())
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch())
   ],
   bootstrap: [App]
 })
--- frontend/src/app/app-routing-module.ts
+++ frontend/src/app/app-routing-module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Dashboard } from './features/dashboard/dashboard';
+import { authGuard, loginGuard } from './core/guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: Dashboard, canActivate: [authGuard] },
+  { path: 'login', loadChildren: () => import('./features/login/login-module').then(m => m.LoginModule), canActivate: [loginGuard] },
+  { path: 'register', loadChildren: () => import('./features/register/register-module').then(m => m.RegisterModule), canActivate: [loginGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
